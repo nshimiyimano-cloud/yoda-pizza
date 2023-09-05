b8b195ae3888a3a56efa2c8c78ff281a549037ae
nshimiyimano-cloud <njeanluc828@gmail.com>
2023-09-05 09:35:41 +0200

    finished work on our place secrion and its styling

--- src/components/home/OurPlaces.jsx
+++ src/components/home/OurPlaces.jsx
@@ -10,36 +10,37 @@ import cardimage4  from '../../assets/Images/Captura-de-tela-2022-10-27-203624.j
 function OurPlaces() {
   return (
     <>
-   <section className='our-place'>
-    <h2 style={{color:'#095c91ff;'}}>Our Place</h2>
-    <h6 style={{color:' #e11736ff;'}}>Serving Great Pizza From Our Family To Yours</h6>
-    <div className='d-flex justify-content-evenly align-items-center flex-row'>
-      <div className='image-box'>
-      <img src="" alt={razaimage2} />
+   <section className='our-place' >
+    <div className='service-wrapper' style={{margin:'auto',width:'95%'}}>
+    <h2 >Our Place</h2>
+    <h6>Serving Great Pizza From Our Family To Yours</h6>
+    <div className='row my-5'>
+      <div className='image-box col col-xs-12 col-sm-12 col-md-4 col-lg-4'>
+      <img src={razaimage2}  alt=""/>
      <h3>Our restaurant</h3>
      <p>The perfect place to dine and spend your afternoons.</p>
 
       </div>
 
 
-      <div className='image-box'>
+      <div className='image-box col col-xs-12 col-sm-12 col-md-4 col-lg-4'>
       <img src={cardimage3} alt="" />
      <h3>Stop... and enjoy!</h3>
      <p>Everything you need: Juice, pizzas, coffee, spaghetti, chips and more!</p>
 
       </div>
 
-      <div className='image-box'>
+      <div className='image-box col-xs-12 col-sm-12 col-md-4 col-lg-4'>
       <img src={cardimage4} alt="" />
      <h3>Calm and Peaceful</h3>
      <p>Relax and enjoy, we will do our best to make your experience impeccable.</p>
 
       </div>
 
-      <div className='image-box'>
-      <img src={commentImage1} alt="" />
+      <div className='image-box col col-xs-12 col-sm-12 col-md-4 col-lg-4'>
+      <img style={{objectFit:'cover',height:'350px',width:'100%'}} src={commentImage1} alt="" />
+     <p style={{color:'#0986c0',width:'unset'}}>Google business Site</p>
      
-     <p>Google business Site</p>
 
       </div>
       
@@ -47,7 +48,7 @@ function OurPlaces() {
        
 
     </div>
-
+    </div>
    </section>
 
    </>
